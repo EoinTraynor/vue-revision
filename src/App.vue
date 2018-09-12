3637c94df0f69a78f32b34cdfa057dd573448a62
EoinTraynor <eointraynor1@gmail.com>
2018-09-12 21:22:43 +0100

    slots

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,37 @@
 <template>
     <div>
-        <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-        <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-        <ul>
-          <li v-for="ninja in ninjas">{{ ninja.name }}</li>
-        </ul>
-        <app-footer v-bind:title="title"></app-footer>
+        <form-helper>
+            <div slot="form-header">
+                <h3>Form Header</h3>
+                <p>Info about the form</p>
+            </div>
+            <div slot="form-fields">
+                <input type="text" placeholder="name" required>
+                <input type="password" placeholder="password" required>                
+            </div>
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Submit</button>
+            </div>
+        </form-helper>
     </div>
 </template>
 
 <script>
 // Imports
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Ninjas from './components/Ninjas.vue';
-
+import FormHelper from './components/FormHelper.vue';
 export default {
     components: {
-        'app-header': Header,
-        'app-footer': Footer,
-        'app-ninjas': Ninjas
+        FormHelper,
     },
     data () {
         return {
-          ninjas: [
-              {name: 'Ryu', speciality: 'Vue Components', show: false},
-              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-              {name: 'Hitoshi', speciality: 'Click Events', show: false},
-              {name: 'Tango', speciality: 'Conditionals', show: false},
-              {name: 'Kami', speciality: 'Webpack', show: false},
-              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-          ],
-          title: 'Vue'
+            title: 'I am a slot title'
         }
     },
     methods: {
-      updateTitle: function(updatedTitle){
-        this.title = updatedTitle;
-      }
+        handleSubmit() {
+            console.warn("handleSubmit");
+        }
     }
 }
 </script>
